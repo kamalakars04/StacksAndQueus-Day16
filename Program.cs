98e5ee8347e2315bc07274a4137f0fe259287e93
kamalakars04 <kamalakars04@gmail.com>
2020-10-25 00:22:38 +0530

    [Kamalakar].[Refactor].Added UC1_Queues

--- Program.cs
+++ Program.cs
@@ -6,10 +6,23 @@ namespace StacksAndQueues
     {
         static void Main(string[] args)
         {
+            // Stacks
             Stacks<int> stacks = new Stacks<int>();
             stacks.Push(70);
             stacks.Push(30);
             stacks.Push(56);
+            stacks.Display();
+            stacks.Peek();
+            stacks.Pop();
+            stacks.Display();
+            stacks.IsEmpty();
+            stacks.Display();
+
+            // Queue
+            Queues<int> queue = new Queues<int>();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
         }
     }
 }
---
+++ Queues.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    class Queues<T>
+    {
+        Node<T> head;
+
+        /// <summary>
+        /// UC 1 Enqueues the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        public void Enqueue(T item)
+        {
+            Node<T> new_node = new Node<T>(item);
+            if (this.head == null)
+            {
+                this.head = new_node;
+            }
+            else
+            {
+                Node<T> temp = this.head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = new_node;
+            }
+            Console.WriteLine("inserted into list " + new_node.data);
+        }
+    }
+}
