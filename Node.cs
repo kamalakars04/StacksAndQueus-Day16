805a28137c290e80aa284924b305e0ce2ab78945
kamalakars04 <kamalakars04@gmail.com>
2020-10-24 23:49:18 +0530

    [Kamalakar].[Refactor].Added UC1_AbilityToAddStack

---
+++ Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    class Node<T>
+    {
+        T data;
+        public Node<T> next;
+        public Node(T data)
+        {
+            this.data = data;
+        }
+    }
+}
